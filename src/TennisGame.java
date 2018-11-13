e4a2ff879bf66332d4bd6d46e6091c75c47237f9
mrvdark <m.r.v.leo.dark@gmail.com>
2018-11-13 09:41:15 +0700

    done

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,12 +1,15 @@
 public class TennisGame {
+    public static void main(String[] args) {
+        System.out.println(getScore("Vinh", "an", 4, 7));
+    }
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int point1, int point2) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        int tempScore = 0;
+
+        boolean sameScore = point1 == point2;
+        if (sameScore) {
+            switch (point1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -24,35 +27,35 @@ public class TennisGame {
                     break;
 
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+        } else {
+            boolean result = point1 >= 4 || point2 >= 4;
+            if (result) {
+                int minusResult = point1 - point2;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            } else {
+                for (int i = 1; i < 3; i++) {
+                    if (i == 1) tempScore = point1;
+                    else {
+                        score += "-";
+                        tempScore = point2;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score += "Love";
+                            break;
+                        case 1:
+                            score += "Fifteen";
+                            break;
+                        case 2:
+                            score += "Thirty";
+                            break;
+                        case 3:
+                            score += "Forty";
+                            break;
+                    }
                 }
             }
         }
